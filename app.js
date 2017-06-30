f17db94c129e3357b9e4e75f392a53fa05df5869
Sonia Singla <ssoniyaster@gmail.com>
2017-06-30 20:10:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 
 (function(){
-
+ use strict;
  angular.module( 'LunchCheck' ,[])
    
-.controller(' LunchCheckController ',function(){
-
+.controller(' LunchCheckController ',function($scope){
+  $scope.check = function(){
+    var arr =  $scope.user.split(',');
+    var deep = arr.length;
+    if (deep <=3){
+      $scope.user="Enjoy";
+    
+    }elif(deep>3){
+    
+      $scope.user="Too much!";
+    }elif(deep=""){
+    
+      $scope.user="Please enter data first";
+    }
+    
+     
+  }
 })
 
 
